b5524b7251801b68e7d3f04247cfaca38da61583
Matty <aelliodreww@gmail.com>
2026-02-16 23:41:48 +0630

    fix firebase auth

--- firebase.ts
+++ firebase.ts
@@ -1,8 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtU...",
+  apiKey: "AIzaSyBtU...",   // မင်း API key
   authDomain: "budget-tracker-7cef2.firebaseapp.com",
   projectId: "budget-tracker-7cef2",
   storageBucket: "budget-tracker-7cef2.firebasestorage.app",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
