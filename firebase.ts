52f4543d641d3c243d875e02c3894d8b0ec34e3b
Matty <aelliodreww@gmail.com>
2026-02-16 23:57:21 +0630

    use correct firebase project

--- firebase.ts
+++ firebase.ts
@@ -1,8 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBtU...",
-  authDomain: "budget-tracker-7cef2.firebaseapp.com",
-  projectId: "budget-tracker-7cef2",
-  storageBucket: "budget-tracker-7cef2.firebasestorage.app",
-  messagingSenderId: "622212622310",
-  appId: "1:622212622310:web:79bb..."
+  apiKey: "AIzaSyBQkHm3baCznh-U-4FnxYT-UfKac3DN0U",
+  authDomain: "budget-tracker-38ba8.firebaseapp.com",
+  projectId: "budget-tracker-38ba8",
+  storageBucket: "budget-tracker-38ba8.firebasestorage.app",
+  messagingSenderId: "96399113962",
+  appId: "1:96399113962:web:b4ce71e844bb8522512193"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
