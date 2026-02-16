8256a02dca67d8b166a71a439e9cad13974c3ed3
Matty <aelliodreww@gmail.com>
2026-02-16 23:46:54 +0630

    update

--- firebase.ts
+++ firebase.ts
@@ -1,18 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBtU...",   // မင်း API key
+  apiKey: "AIzaSyBtU...",
   authDomain: "budget-tracker-7cef2.firebaseapp.com",
   projectId: "budget-tracker-7cef2",
   storageBucket: "budget-tracker-7cef2.firebasestorage.app",
   messagingSenderId: "622212622310",
-  appId: "1:622212622310:web:79bb...",
-  measurementId: "G-S49RGYDH2S"
+  appId: "1:622212622310:web:79bb..."
 };
-
-const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
-export const auth = getAuth(app);
